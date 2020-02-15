2bb0056f06d7594e6b4199e63d225c7e2646ce1d
İzni Burak Demirtaş <izniburak@outlook.com>
2020-02-15 13:47:17 +0300

    Updated routes.

--- www/routes/api.php
+++ www/routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('healt-check', 'MainController@healtCheck');
+Route::get('info', 'MainController@info');
 Route::middleware('auth:api')->group(function () {
-    Route::get('healt-check', 'MainController@healtCheck');
-    Route::get('info', 'MainController@info');
     Route::apiResource('users', 'UserController');
     Route::prefix('users')->group(function () {
         Route::get('{song}/favorites', 'UserController@favorites');
